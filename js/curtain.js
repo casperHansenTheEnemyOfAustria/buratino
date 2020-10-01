db84fd9377287cfdac1ff936120541711d9e406d
rony-majzoub-nti-johanneberg <60172919+rony-majzoub-nti-johanneberg@users.noreply.github.com>
2020-10-02 01:18:30 +0200

    SCSS, JS & Icons
    Rewritten CSS to SCSS. The SCSS is now structured into multiple files, for a more modular approach to development. The Icons for the main tiles have been imported from the Figma-prototype. Lastly, some more JavaScript has been added, to replicate certain behavior seen in the Figma-prototype.

--- js/curtain.js
+++ js/curtain.js
@@ -3,4 +3,9 @@ function curtainDown() {
     targetElement.className = "roll-down";
     var targetElement = document.getElementById("curtain-grid");
     targetElement.className = "tile-transition";
+    
   }
+
+function wifiSwitch() {
+  document.getElementById("long-button-text").innerHTML = "TRYCK HÄR FÖR WIFI";
+}
