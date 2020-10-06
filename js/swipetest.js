6f34b4f40ac15aec3b54a47ac6ea4f3ccb3fe670
casper-hansen <repsac2002@gmail.com>
2020-10-06 21:59:00 +0200

    Fixed grid and button animation
    also made the code look neater :)

--- js/swipetest.js
+++ js/swipetest.js
@@ -1,6 +1,28 @@
-var targetElement1 = document.getElementById("curtain");
+// this program is used to detect a downward swipe on the curtain and to then 
+// activate the curtain animation
 
-var targetElement2 = document.getElementById("curtain-grid");
+// sets dead zone in css-pixels for the swipe
+var sens = 0
+
+
+
+// defines what transitions to use
+function curtainDown() {
+    var targetElement = document.getElementById("curtain");
+    targetElement.className = "roll-down";
+    var targetElement = document.getElementById("curtain-grid");
+    targetElement.className = "tile-transition";   
+  }
+
+function wifiSwitch() {
+  document.getElementById("long-button-text").innerHTML = "TRYCK HÄR FÖR WIFI";
+}
+
+var curtain = document.getElementById("curtain");
+
+var curtainGrid = document.getElementById("curtain-grid");
+
+
 
 
 // looks for y coord of cursor at click and compares to cursor coord at release
@@ -9,42 +31,41 @@ var y1 = 0
 var y2 = 0
 
 // for desktop
-    // targetElement1.addEventListener("mousedown", function(e) {
+    // curtain.addEventListener("mousedown", function(e) {
     //     y1 = e.pageY
     //     console.log(y1);
         
     // });
 
     
-    // targetElement1.addEventListener("mouseup", function(e) {
+    // curtain.addEventListener("mouseup", function(e) {
     //     y2 = e.pageY;
     //     console.log(y2);
     //     if(y1 < y2){
-    //         targetElement1.className = "roll-down"
-    //         targetElement2.className = "title-transition"
+    //         curtainDown();
+    //         wifiSwitch() 
             
     //     }
     // });
 
 // for phones
-    targetElement1.addEventListener("touchstart", function(e) {
+    curtain.addEventListener("touchstart", function(e) {
         y1 = e.touches[0].pageY
         console.log(y1);
         
     });
 
     var endCoord = {}
-    targetElement1.addEventListener("touchmove", function(e) {
+    curtain.addEventListener("touchmove", function(e) {
         endCoord = e.targetTouches[0];
     });
     
-    targetElement1.addEventListener("touchend", function(e) {
+    curtain.addEventListener("touchend", function(e) {
         y2 = endCoord.pageY;
         console.log(e);
-        if(y2 > y1){
-            targetElement1.className = "roll-down"
-            targetElement2.className = "title-transition"
-            
+        if(y2-y1 > sens){
+            curtainDown();
+            wifiSwitch()   
         }
     });
 
