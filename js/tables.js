e74cee032faa0eacfd6a4d5c061ae04daecaf0ab
casper-hansen <repsac2002@gmail.com>
2020-11-06 14:32:24 +0100

    Further adding to furniture map

--- js/tables.js
+++ js/tables.js
@@ -1,68 +1,82 @@
-var dragItem = document.querySelector(".table");
-var container = document.querySelector(".table");
+function tables(number){
+  console.log("running on" + number)
+  var dragItem = document.querySelector(".table" + number);
+  var containers = document.querySelectorAll(".table" + number)
 
-    var active = false;
-    var currentX;
-    var currentY;
-    var initialX;
-    var initialY;
-    var xOffset = 0;
-    var yOffset = 0;
-// add a function around this with the container sumber as a var for later use with multiple tables
-    container.addEventListener("touchstart", dragStart, false);
-    container.addEventListener("touchend", dragEnd, false);
-    container.addEventListener("touchmove", drag, false);
 
-    container.addEventListener("mousedown", dragStart, false);
-    container.addEventListener("mouseup", dragEnd, false);
-    container.addEventListener("mousemove", drag, false);
+  dragItem.classList.add("selected")
+      var active = false;
+      var currentX;
+      var currentY;
+      var initialX;
+      var initialY;
+      var xOffset = 0;
+      var yOffset = 0;
+  // add a function around this with the container sumber as a var for later use with multiple tables
+  containers.forEach( container => {
+      container.addEventListener("touchstart", dragStart, false);
+      container.addEventListener("touchend", dragEnd, false);
+      container.addEventListener("touchmove", drag, false);
+      container.addEventListener("mousedown", dragStart, false);
+      container.addEventListener("mouseup", dragEnd, false);
+      container.addEventListener("mousemove", drag, false);
+  });
 
-    function dragStart(e) {
-      // check pos at start
-      if (e.type === "touchstart") {
-        initialX = e.touches[0].clientX - xOffset;
-        initialY = e.touches[0].clientY - yOffset;
-      } else {
-        initialX = e.clientX - xOffset;
-        initialY = e.clientY - yOffset;
-      }
 
-      if (e.target === dragItem) {
-        active = true;
-      }
-    }
 
-    function dragEnd(e) {
-      // leave pos at end
-      initialX = currentX;
-      initialY = currentY;
 
-      active = false;
-    }
-
-    function drag(e) {
-      if (active) {
-      
-        // adds this to make it so you dot accidentally scroll
-        e.preventDefault();
-      
-        if (e.type === "touchmove") {
-          // moves if you move yo fingah
-          currentX = e.touches[0].clientX - initialX;
-          currentY = e.touches[0].clientY - initialY;
+      function dragStart(e) {
+        // check pos at start
+        if (e.type === "touchstart") {
+          initialX = e.touches[0].clientX - xOffset;
+          initialY = e.touches[0].clientY - yOffset;
         } else {
-          currentX = e.clientX - initialX;
-          currentY = e.clientY - initialY;
+          initialX = e.clientX - xOffset;
+          initialY = e.clientY - yOffset;
         }
 
-        xOffset = currentX;
-        yOffset = currentY;
+        if (e.target === dragItem) {
+          active = true;
+        }
+      }
 
-        setTranslate(currentX, currentY, dragItem);
+      function dragEnd(e) {
+        // leave pos at end
+        initialX = currentX;
+        initialY = currentY;
+
+        active = false;
+      }
+
+      function drag(e) {
+        if (active) {
+        
+          // adds this to make it so you dot accidentally scroll
+          e.preventDefault();
+        
+          if (e.type === "touchmove") {
+            // moves if you move yo fingah
+            currentX = e.touches[0].clientX - initialX;
+            currentY = e.touches[0].clientY - initialY;
+          } else {
+            currentX = e.clientX - initialX;
+            currentY = e.clientY - initialY;
+          }
+
+          xOffset = currentX;
+          yOffset = currentY;
+
+          setTranslate(currentX, currentY, dragItem);
+        }
+      }
+
+      function setTranslate(xPos, yPos, el) {
+        // adds all the positioning data togetherto make it do its thing
+        el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
       }
     }
 
-    function setTranslate(xPos, yPos, el) {
-      // adds all the positioning data togetherto make it do its thing
-      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+    function selected() {
+
     }
+    function rotation(){}
