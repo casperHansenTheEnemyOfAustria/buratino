924c3d1e78ecd82290fe1250645d14137e058f9c
casper-hansen <repsac2002@gmail.com>
2020-11-30 14:05:53 +0100

    trying remvoing yt js file

--- js/youtubeButtons.js
+++ js/youtubeButtons.js
@@ -1,40 +1,40 @@
-// global variable for the player
-var player = document.querySelector("");
+// '// global variable for the player
+// var player = document.querySelector("");
 
-// this function gets called when API is ready to use
-function onYouTubePlayerAPIReady() {
-    // create the global player from the specific iframe (#video)
-    player = new YT.Player('video', {
-        events: {
-            // call this function when player is ready to use
-            'onReady': onPlayerReady
-        }
-    });
-}
+// // this function gets called when API is ready to use
+// function onYouTubePlayerAPIReady() {
+//     // create the global player from the specific iframe (#video)
+//     player = new YT.Player('video', {
+//         events: {
+//             // call this function when player is ready to use
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
 
-onPlayerReady()
-function onPlayerReady(event) {
+// onPlayerReady()
+// function onPlayerReady(event) {
 
-    // bind events
-    var playButton = document.querySelector(".play-button");
-    playButton.addEventListener("click", function() {
-        player.playVideo();
-    });
+//     // bind events
+//     var playButton = document.querySelector(".play-button");
+//     playButton.addEventListener("click", function() {
+//         player.playVideo();
+//     });
 
-    var pauseButton = document.getElementById("pause-button");
-    pauseButton.addEventListener("click", function() {
-        player.pauseVideo();
-    });
+//     var pauseButton = document.getElementById("pause-button");
+//     pauseButton.addEventListener("click", function() {
+//         player.pauseVideo();
+//     });
 
-    var stopButton = document.getElementById("stop-button");
-    stopButton.addEventListener("click", function() {
-        player.stopVideo();
-    });
+//     var stopButton = document.getElementById("stop-button");
+//     stopButton.addEventListener("click", function() {
+//         player.stopVideo();
+//     });
 
-}
+// }
 
-// Inject YouTube API script
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// // Inject YouTube API script
+// var tag = document.createElement('script');
+// tag.src = "https://www.youtube.com/player_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);'
