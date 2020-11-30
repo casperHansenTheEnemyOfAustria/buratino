03b4a88d1f6f6bd479d546b414f290c0ad5cdee1
casper-hansen <repsac2002@gmail.com>
2020-11-30 15:52:59 +0100

    Further testing for youtube

--- js/videoInject.js
+++ js/videoInject.js
@@ -1,9 +1,7 @@
-var video_wrapper = document.querySelector('.frame');
-//  Check to see if youtube wrapper exists
 
-// If user clicks on the video wrapper load the video.
-function inject(id){
-/* Dynamically inject the iframe on demand of the user.
- Pull the youtube url from the data attribute on the wrapper element. */
+
+function inject(id, number){
+    var video_wrapper = document.querySelector('.frame'+number);
+
     video_wrapper.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/'+id+'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 }
