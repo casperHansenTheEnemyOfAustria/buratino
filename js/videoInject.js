c73ae4a32eadf670a1b693615b1b258024779225
casper-hansen <repsac2002@gmail.com>
2021-01-20 10:56:34 +0100

    revamped autoplay

--- js/videoInject.js
+++ js/videoInject.js
@@ -32,6 +32,6 @@ function videos(num){
     var video_wrapper = document.querySelector('.frame'+num); 
     resetVideos()
     // this puts the vibeo embed wherever its told to
-    video_wrapper.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/'+id+'?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    video_wrapper.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/'+id+'?autoplay=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 }
 
