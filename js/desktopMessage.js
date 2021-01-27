dc6a0e676f3e65fa74c609c66eb6d711af656245
casper-hansen <repsac2002@gmail.com>
2021-01-27 09:44:45 +0100

    Fixed map message to work better and to have better scaling :)

--- js/desktopMessage.js
+++ js/desktopMessage.js
@@ -1,11 +1,17 @@
 var user = detect.parse(navigator.userAgent);
+var map = document.querySelector(".map-ovl")
 
 if(user.os.name == "Windows 7" || user.os.name == "Windows 8" || user.os.name == "Windows 10" || user.os.name == "Mac OS"){
  console.log("this is a desktop")
  var message = document.querySelector(".map-message")
- message.classList.remove("hide")
+ message.classList.add("two")
+ map.classList.remove("two")
+ map.classList.remove("overlay")
+ map.classList.add("hide")
 }
 else{
     var message = document.querySelector(".map-message")
     message.classList.add("hide")
+    
+
 }
--- js/pwaSpecifics.js
+++ js/pwaSpecifics.js
@@ -1,9 +1,20 @@
 let deferredPrompt;
-
+var user = detect.parse(navigator.userAgent);
 window.addEventListener('beforeinstallprompt', (e) => {
 //   prevents the top thingy from appearing
   
   deferredPrompt = e;
 //   promotes installing the pwa
+  
+
   showInstallPromotion();
 });
+if(user.os.name != "Windows 7" || user.os.name != "Windows 8" || user.os.name != "Windows 10" || user.os.name != "Mac OS"){
+  console.log("this is not a desktop")
+  if(user.browser.family == "Mobile Safari" || user.browser.family == "Chrome Mobile iOS" ){
+    console.log("detected apple")
+    if(!(( "standalone" in window.navigator) && window.navigator.standalone)){
+      console.log("detected not installed")   
+    }
+  }
+}
