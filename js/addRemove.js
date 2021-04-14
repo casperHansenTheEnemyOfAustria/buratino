90fa5ee157679fdf4d91b2b601cf90541422ebc7
casper-hansen <repsac2002@gmail.com>
2021-04-14 09:39:19 +0200

    Fixed it all

---
+++ js/addRemove.js
@@ -0,0 +1,114 @@
+// TODO add what happens if max tables reached
+
+var table = document.querySelectorAll("table")
+var selected = document.querySelector(".selected")
+var addRound = document.querySelector(".add-round")
+var addRect = document.querySelector(".add-rect")
+var current = document.querySelectorAll("table")
+var i1 = 1
+var i2 = 17
+var recentlyDeleted 
+var deleted
+var deleted1
+var deleted2
+var temp
+function plusMinus(operator){
+    
+    if(operator == 2){
+        temp = i1
+        // checks if it has been deleted recently and if it is a square
+        if(recentlyDeleted <=15 && deleted1 == true){
+            // makes the next one to be shown the same as the recently deleted one
+            i1 = recentlyDeleted
+        }
+        
+        // for adding square
+        if(i1 <= 15){
+            
+            addTable(i1)
+            if(deleted1 == true){
+                // goes bac kone if it has already shown one before
+                i1 = temp - 1
+            }
+            else{
+                // makes the item the stored one beforee things were deleted
+                i1 = temp
+            }
+            // resets if something was deleted or not
+            deleted1 = false
+            // goes on to the next table
+            i1 = i1 + 1   
+        }
+        else{
+            // goes back to the first table
+            i1 = 1
+            // add what happens if there are max tables here
+            console.log("max square tables")
+        };
+        
+    }
+    else if(operator == 1){
+        temp = i2
+        // checks if it has been deleted recently and if it is a circle
+        if(recentlyDeleted <=20 && deleted2== true){
+            
+            i2 = recentlyDeleted
+        }
+        if(i2 <= 20){ 
+            // for adding circle
+            addTable(i2)
+            if(deleted2 == true){
+                // assigns the store value -1 since last shownb table wasnt the one before this one
+                i2 = temp - 1
+            }
+            else{
+                // assigns the stored value back to the iterator
+                i2 = temp
+            }
+            deleted2 = false
+            
+            i2 = i2 + 1
+        }
+        else{
+            // add what happens if there are max tables here
+            console.log("max circle tables")
+            // goes back to the first table
+            i2 = 17
+        }
+    }
+    else if(operator == 0){
+        // runs remove table function
+        removeTable()
+        
+    }
+}
+
+function addTable(number){
+    n = number.toString();
+    console.log("at"+n)
+    current = document.querySelector(".table"+n)
+    console.log("showing table"+n)
+    current.classList.remove("hide")        
+}
+
+function removeTable(){
+    var selected = document.querySelector(".selected")
+    selected.classList.add("hide")
+    // takes the id of the recetly deleted table
+    name = selected.id
+    // stores the id as int
+    recentlyDeleted = parseInt(name)
+
+    if(recentlyDeleted <=15){
+        // checks if square or circle
+        deleted1 = true
+    }
+    else{
+        deleted2 = true
+    }
+    // this gets used by the show func to show the one recently deleted
+    console.log("class name"+name)
+}
+
+// addRound.addEventListener("click", plusMinus(1))
+// addRect.addEventListener("click", plusMinus(2))
--- js/overlays.js
+++ js/overlays.js
@@ -19,6 +19,9 @@ function showbg(){
 var one = document.querySelectorAll(".one");
 var two = document.querySelectorAll(".two");
 var three = document.querySelectorAll(".three");
+var four = document.querySelectorAll(".four");
+var five = document.querySelectorAll(".five");
+var six = document.querySelectorAll(".six");
 
 // had to make these weirrd functions to use for every single overlay/buttonm on every page but now it works beautifully 
  // shows overlays
@@ -26,16 +29,38 @@ function showone(){
     one.forEach(element => {
         element.classList.remove("hide")
     });
+    window.scrollTo(0,0);
+    
 }
 function showtwo(){
     two.forEach(element => {
         element.classList.remove("hide")
     });
+    window.scrollTo(0,0);
 }
 function showthree(){
     three.forEach(element => {
         element.classList.remove("hide")
     });
+    window.scrollTo(0,0);
+}
+function showfour(){
+    four.forEach(element => {
+        element.classList.remove("hide")
+    });
+    window.scrollTo(0,0);
+}
+function showfive(){
+    five.forEach(element => {
+        element.classList.remove("hide")
+    });
+    window.scrollTo(0,0);
+}
+function showsix(){
+    six.forEach(element => {
+        element.classList.remove("hide")
+    });
+    window.scrollTo(0,0);
 }
 
 // hides overlays
@@ -54,6 +79,21 @@ function hidethree(){
         element.classList.add("hide")
     });
 }
+function hidefour(){
+    four.forEach(element => {
+        element.classList.add("hide")
+    });
+}
+function hidefive(){
+    five.forEach(element => {
+        element.classList.add("hide")
+    });
+}
+function hidesix(){
+    six.forEach(element => {
+        element.classList.add("hide")
+    });
+}
 
 
 // these are the different buttons on the site oderered from top to bottom. add this class to anything yuo want to
@@ -61,6 +101,9 @@ function hidethree(){
 var btn1 = document.querySelectorAll(".btn1")
 var btn2 = document.querySelectorAll(".btn2")
 var btn3 = document.querySelectorAll(".btn3")
+var btn4 = document.querySelectorAll(".btn4")
+var btn5 = document.querySelectorAll(".btn5")
+var btn6 = document.querySelectorAll(".btn6")
 
 
 
@@ -74,6 +117,9 @@ function resetOvls(){
     hideone()
     hidetwo()
     hidethree()
+    hidefour()
+    hidefive()
+    hidesix()
     document.getElementById("slide-box").classList.remove("hide")
 };
 
@@ -105,7 +151,30 @@ function showOvl(which){
         showbg()
         document.getElementById("slide-box").classList.add("hide")
     }
-
+    if(which == "4"){
+        resetOvls();
+        // shows the specific overlay depending on what you input into the function
+        showfour()
+        console.log("showing OVL")
+        showbg()
+        document.getElementById("slide-box").classList.add("hide")
+    }
+    if(which == "5"){
+        resetOvls();
+        // shows the specific overlay depending on what you input into the function
+        showfive()
+        console.log("showing OVL")
+        showbg()
+        document.getElementById("slide-box").classList.add("hide")
+    }
+    if(which == "6"){
+        resetOvls();
+        // shows the specific overlay depending on what you input into the function
+        showsix()
+        console.log("showing OVL")
+        showbg()
+        document.getElementById("slide-box").classList.add("hide")
+    }
 }
 
 // these are the event listeners that execute the code on click
@@ -122,6 +191,21 @@ btn2.forEach(element => {
 btn3.forEach(element => {
     element.addEventListener("click", function(){
         showOvl("3")
+    })
+});
+btn4.forEach(element => {
+    element.addEventListener("click", function(){
+        showOvl("4")
+    })
+});
+btn5.forEach(element => {
+    element.addEventListener("click", function(){
+        showOvl("5")
+    })
+});
+btn6.forEach(element => {
+    element.addEventListener("click", function(){
+        showOvl("6")
     })
 });
 
@@ -143,5 +227,12 @@ wifiClose.forEach( element => {
     element.addEventListener("click", function(){
         document.getElementById("slide-box").classList.add("hide")
         console.log("added hide to slide box")
+        // scrolls back to the initial position for iphones 
+        if(user.browser.family == "Mobile Safari" || user.browser.family == "Chrome Mobile iOS"){
+            if(!(( "standalone" in window.navigator) && window.navigator.standalone)){
+                window.scrollTo(0,25)
+                console.log("ew youre using an apple device")
+            }
+        }
     })
 });
